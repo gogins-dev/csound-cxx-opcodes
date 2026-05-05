dc3c81141f76b9d892f40778fa050709207f0d24
gogins <michael.gogins@gmail.com>
2026-05-05 09:21:50 -0400

    Porting to Csound 7.

--- clean-build.sh
+++ clean-build.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 echo "Creating clean build..."
-git submodule update --init --recursive
-git submodule status --recursive
-cd csound
-git checkout csound6
-git branch
-git pull
-cd ..
 rm -rf build/*
 mkdir -p build
 cd build
